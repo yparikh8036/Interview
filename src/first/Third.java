01469014a1b0e6647d305205cb6e74e8600f670f
yashparikh <yash.150410107061@gmail.com>
2021-10-27 16:44:46 -0400

    Trial

--- src/first/Third.java
+++ src/first/Third.java
@@ -1,6 +1,6 @@
 package first;
 
-//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+// Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
 
 import java.util.Scanner;
 
